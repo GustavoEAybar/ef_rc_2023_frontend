9ed8561d8151841828985daf64d5582a23006d91
GustavoExequielAybar <aybargustavoe@gmail.com>
2023-11-23 16:18:26 -0300

    install dependencies

---
+++ src/components/Heading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const heading = () => {
+    return (
+        <div>
+            <h1 className='text-center lead'>Entrega Final 2023 - Rolling Code School</h1>
+            <hr/>
+        </div>
+    );
+};
+
+export default heading;
---
+++ src/components/Subheading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Subheading = () => {
+    return (
+        <div>
+            <h1 className='display-4 text-center'>Bienvenido!</h1>
+        </div>
+    );
+};
+
+export default Subheading;
