48dd09f18999810b90ae132a51d939f80f233ac1
Gerardo Rosales <rosalesgerardoruben@gmail.com>
2023-12-05 22:24:02 -0300

    basic folder creation contact 051223 cm/2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Heading from './components/Heading';
 import Subheading from './components/Subheading';
+import Contact from './components/contact/Contact';
+
 
 function App() {
   return (
     <div>
       <Heading/>
       <Subheading/>
+      <Contact/>
     </div>
   );
 }
---
+++ src/components/contact/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>CONTACTO</h1>
+      <hr/>
+      <h2>Obtenga Resultados Reales!!!</h2>
+      <br/>
+      <h2>Contactenos</h2>
+      <img src={require('./imgcontacto1.png')}/>
+    </div>
+  )
+}
+
+export default Contact
