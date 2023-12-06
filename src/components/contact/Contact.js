918e970be54f7cfb7e526c00f8f157ebe7f2a45f
Gerardo Rosales <rosalesgerardoruben@gmail.com>
2023-12-05 23:30:28 -0300

    modification page contact 051223 c/3

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import styles from "./contact.css"
 
 const Contact = () => {
   return (
-    <div>
+    <div className={styles.fondo}>
       <h1>CONTACTO</h1>
       <hr/>
       <h2>Obtenga Resultados Reales!!!</h2>
       <br/>
       <h2>Contactenos</h2>
-      <img src={require('./imgcontacto1.png')}/>
+      <img src={require('./imgcontacto1.png')} alt='Imagen Hombre Haciendo Peso Muerto'/>
     </div>
   )
 }
